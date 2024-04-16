53d8100be748504d5adc7e46bf507ce83998d655
mytkom <marek.mytkowski.mm@gmail.com>
2024-04-16 21:47:03 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Nix project environment with zig";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          in
+          {
+            default = with pkgs; mkShell.override { stdenv = gcc12Stdenv; }
+              {
+                shellHook = ''
+                  zellij
+                '';
+                packages = [
+                  zig
+                ];
+                buildInputs = [
+                  libGL
+                  xorg.libX11.dev
+                  xorg.libXcursor.dev
+                  xorg.libXrandr.dev
+                  xorg.libXinerama.dev
+                  xorg.libXi.dev
+                ];
+              };
+          }
+        );
+    };
+}
