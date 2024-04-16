da2573f3a72a4df89b806c94f6ff32a06dc4cc79
mytkom <marek.mytkowski.mm@gmail.com>
2024-04-16 20:51:33 +0200

    init

---
+++ src/main.zig
@@ -0,0 +1,13 @@
+const raylib = @cImport(@cInclude("raylib.h"));
+
+pub fn main() !void {
+	raylib.InitWindow(960, 540, "Mr. Pralinka");
+	raylib.SetTargetFPS(144);
+	defer raylib.CloseWindow();
+
+	while (!raylib.WindowShouldClose()) {
+		raylib.BeginDrawing();
+		raylib.ClearBackground(raylib.BLACK);
+		raylib.EndDrawing();
+	}
+}
