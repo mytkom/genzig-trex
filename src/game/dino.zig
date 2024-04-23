16148e11256d627173605aaac02baf6fb57f5669
Willu12 <boryskurdek@gmail.com>
2024-04-24 00:57:40 +0200

    added jump action

--- src/game/dino.zig
+++ src/game/dino.zig
@@ -1,8 +1,11 @@
 const raylib = @cImport(@cInclude("raylib.h"));
 const zigTime = @import("std").time;
+const fp_ms = @import("game.zig").fp_ms;
 
 const jump_height = 100;
 const jump_time = 3 * 1000_000;
+const jump_dist_frame = jump_height * fp_ms / jump_time;
+const dino_speed = 1;
 
 const jump = struct { jump_start_time: i64, is_jumping: bool };
 
@@ -20,7 +23,16 @@ pub const dinosaur = struct {
         self.ducking = true;
     }
 
-    fn 
+    fn move_right(self: *dinosaur, frames_to_process: i32) void {
+        self.position.x += frames_to_process * dino_speed;
+    }
+    fn move_up(self: *dinosaur, frames_to_process: i32) void {
+        self.position.y += frames_to_process * jump_dist_frame;
+    }
+
+    fn move_down(self: *dinosaur, frames_to_process: i32) void {
+        self.position.y -= frames_to_process * jump_dist_frame;
+    }
 
     fn process_jump(self: *dinosaur, frames_to_process: i32) void {
         if (self.jump.is_jumping == false) return;
@@ -28,9 +40,12 @@ pub const dinosaur = struct {
         var current_jump_time = zigTime.timestamp() - self.jump.jump_start_time;
         if (current_jump_time > jump_time) {
             self.jump.is_jumping = false;
+            self.position.y = 0;
             return;
         } else if (current_jump_time < jump_time) {
-
+            move_up(self, frames_to_process);
+        } else {
+            move_down(self, frames_to_process);
         }
     }
 };
@@ -41,7 +56,7 @@ pub fn create_dinosaur() dinosaur {
         .height = 100,
         .width = 10,
         .ducking = false,
-        .jump = jump{ .current_jump_time = 0, .is_jumping = false },
+        .jump = jump{ .jump_start_time = 0, .is_jumping = false },
     };
     return dino;
 }
--- src/game/game.zig
+++ src/game/game.zig
@@ -1,10 +1,11 @@
 const dino = @import("dino.zig");
 const zigTime = @import("std").time;
 
+pub const fp_ms = 60 * 1000_000;
+
 const game_data = struct {
     time_ms: i64,
     dino: dino.dinosaur,
-    fp_ms: i64,
     start_time: i64,
 
     fn update_game_time(self: *game_data) i64 {
@@ -21,13 +22,12 @@ pub fn run_game() void {
     var game = game_data{
         .time_ms = 0,
         .dino = dino.create_dinosaur(),
-        .fp_ms = 30 * 1000_000,
         .start_time = zigTime.timestamp(),
     };
 
     while (true) {
         var dt = game.update_game_time();
-        var frames_to_compute = @divFloor(dt, game.fp_ms);
-        dt -= frames_to_compute * game.fp_ms;
+        var frames_to_compute = @divFloor(dt, fp_ms);
+        dt -= frames_to_compute * fp_ms;
     }
 }
