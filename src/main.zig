79e3b2e1d97154736227a2503960e44879bd1e3a
mytkom <marek.mytkowski.mm@gmail.com>
2024-04-27 08:59:40 +0200

    first sprite render

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,22 @@
 const raylib = @cImport(@cInclude("raylib.h"));
 
 pub fn main() !void {
-    raylib.InitWindow(960, 540, "Mr. Pralinka");
+    const initScreenWidth = 960;
+    const initScreenHeight = 540;
+
+    raylib.InitWindow(initScreenWidth, initScreenHeight, "Mr. Pralinka");
     raylib.SetTargetFPS(144);
     defer raylib.CloseWindow();
+
+    const sprite = raylib.LoadTexture("resources/sprite.png");
+    const dino = raylib.Rectangle{ .x = 30, .y = 0, .width = 60, .height = 50 };
 
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
         raylib.ClearBackground(raylib.BLACK);
+
+        raylib.DrawTextureRec(sprite, dino, raylib.Vector2{ .x = 0, .y = 0 }, raylib.WHITE);
+
         raylib.EndDrawing();
     }
 }
