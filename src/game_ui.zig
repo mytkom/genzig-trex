a21a044f12a2d0beccbe531cc3a3ffae50e89c45
mytkom <marek.mytkowski.mm@gmail.com>
2024-05-02 07:10:33 +0200

    add working ground

--- src/game_ui.zig
+++ src/game_ui.zig
@@ -10,11 +10,126 @@ pub const DinoTextures = struct {
     dead: raylib.Rectangle,
 };
 
-pub const GameUIConfig = struct { sprite: raylib.Texture2D, width: i32, height: i32, dino: DinoTextures, dinoPosition: raylib.Vector2 };
+pub const GameUIConfig = struct {
+    sprite: raylib.Texture2D,
+    width: i32,
+    height: i32,
+    scaleFactor: f32,
+    dino: DinoTextures,
+    dinoPosition: raylib.Vector2,
+    groundTexture: raylib.Rectangle,
+};
 
-pub fn DrawMenu(config: GameUIConfig) void {
-    const scale = @as(f32, @floatFromInt(config.width)) / 8.0 / config.dino.standing.height;
-    const destination = raylib.Rectangle{ .x = config.dinoPosition.x, .y = config.dinoPosition.y, .width = config.dino.menuIcon.width * scale, .height = config.dino.menuIcon.height * scale };
-    raylib.DrawTexturePro(config.sprite, config.dino.menuIcon, destination, raylib.Vector2{ .x = 0, .y = 0 }, 0, raylib.WHITE);
-    raylib.DrawText("Press space to play", @intFromFloat(config.dinoPosition.x), @intFromFloat(config.dinoPosition.y + (config.dino.standing.height * scale)), 30, raylib.BLACK);
+pub const ObstacleType = enum {
+    Cactus,
+    Pterodile,
+};
+
+pub const Obstacle = struct {
+    rect: raylib.Rectangle,
+    type: ObstacleType,
+};
+
+pub const Ground = struct {
+    lines: *[2]raylib.Vector2,
+};
+
+pub const Scene = struct {
+    obstacles: []Obstacle,
+    ground: Ground,
+};
+
+pub const GameState = enum {
+    startScreen,
+    running,
+    gameOver,
+};
+
+pub fn DrawUI(state: *GameState, config: *GameUIConfig, scene: *Scene) void {
+    if (raylib.IsKeyDown(raylib.KEY_SPACE) and state.* == GameState.startScreen) {
+        state.* = GameState.running;
+    }
+
+    switch (state.*) {
+        GameState.startScreen => {
+            DrawMenu(config);
+        },
+        GameState.running => {
+            DrawScene(config, scene);
+        },
+        GameState.gameOver => {},
+    }
+}
+
+fn DrawMenu(config: *const GameUIConfig) void {
+    const destination = raylib.Rectangle{
+        .x = config.dinoPosition.x,
+        .y = config.dinoPosition.y,
+        .width = config.dino.menuIcon.width * config.scaleFactor,
+        .height = config.dino.menuIcon.height * config.scaleFactor,
+    };
+    raylib.DrawTexturePro(
+        config.sprite,
+        config.dino.menuIcon,
+        destination,
+        raylib.Vector2{ .x = 0, .y = 0 },
+        0,
+        raylib.WHITE,
+    );
+    raylib.DrawText(
+        "Press space to play",
+        @intFromFloat(config.dinoPosition.x),
+        @intFromFloat(config.dinoPosition.y + (config.dino.standing.height * config.scaleFactor)),
+        30,
+        raylib.BLACK,
+    );
+}
+
+fn DrawScene(config: *const GameUIConfig, scene: *Scene) void {
+    for (scene.ground.lines) |line| {
+        const groundLineDestination = raylib.Rectangle{
+            .x = line.x,
+            .y = line.y,
+            .width = config.groundTexture.width * config.scaleFactor,
+            .height = config.groundTexture.height * config.scaleFactor,
+        };
+
+        raylib.DrawTexturePro(
+            config.sprite,
+            config.groundTexture,
+            groundLineDestination,
+            raylib.Vector2{ .x = 0, .y = 0 },
+            0,
+            raylib.WHITE,
+        );
+    }
+
+    const dinoDestination = raylib.Rectangle{
+        .x = config.dinoPosition.x,
+        .y = config.dinoPosition.y,
+        .width = config.dino.standing.width * config.scaleFactor,
+        .height = config.dino.standing.height * config.scaleFactor,
+    };
+
+    raylib.DrawTexturePro(
+        config.sprite,
+        config.dino.standing,
+        dinoDestination,
+        raylib.Vector2{ .x = 0, .y = 0 },
+        0,
+        raylib.WHITE,
+    );
+}
+
+pub fn UpdateScene(scene: *Scene, config: *GameUIConfig, deltaTime: f32) void {
+    const step: i32 = 100;
+
+    for (scene.ground.lines) |*line| {
+        line.x -= step * deltaTime;
+    }
+
+    if (scene.ground.lines[0].x <= -config.groundTexture.width * config.scaleFactor) {
+        scene.ground.lines[0].x = 0;
+        scene.ground.lines[1].x = config.groundTexture.width * config.scaleFactor;
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -2,33 +2,57 @@ const raylib = @cImport(@cInclude("raylib.h"));
 const gameUI = @import("game_ui.zig");
 
 pub fn main() !void {
-    const initWindowWidth: i32 = 960;
-    const initWindowHeight: i32 = 540;
+    const desiredGameWidth: f32 = 400;
+    const initWindowWidth: f32 = 960;
+    const initWindowHeight: f32 = 540;
     raylib.InitWindow(initWindowWidth, initWindowHeight, "Chrome dino game");
     defer raylib.CloseWindow();
 
-    raylib.SetTargetFPS(144);
+    raylib.SetTargetFPS(60);
 
-    const UIConfig = gameUI.GameUIConfig{
+    var UIConfig = gameUI.GameUIConfig{
         .width = initWindowWidth,
         .height = initWindowHeight,
-        .dinoPosition = raylib.Vector2{ .x = initWindowWidth / 12, .y = initWindowHeight / 2 },
+        .scaleFactor = initWindowWidth / desiredGameWidth,
+        .dinoPosition = .{ .x = initWindowWidth / 12, .y = initWindowHeight / 2 },
         .sprite = raylib.LoadTexture("resources/sprite.png"),
-        .dino = gameUI.DinoTextures{
-            .standing = raylib.Rectangle{ .x = 848, .y = 2, .width = 43, .height = 46 },
-            .dead = raylib.Rectangle{ .x = 1068, .y = 2, .width = 45, .height = 50 },
-            .leftStep = raylib.Rectangle{ .x = 936, .y = 2, .width = 45, .height = 50 },
-            .rightStep = raylib.Rectangle{ .x = 980, .y = 2, .width = 45, .height = 50 },
-            .crouchLeftStep = raylib.Rectangle{ .x = 1112, .y = 20, .width = 58, .height = 28 },
-            .crouchRightStep = raylib.Rectangle{ .x = 1170, .y = 20, .width = 58, .height = 28 },
-            .menuIcon = raylib.Rectangle{ .x = 40, .y = 4, .width = 43, .height = 46 },
+        .dino = .{
+            .standing = .{ .x = 848, .y = 2, .width = 43, .height = 46 },
+            .dead = .{ .x = 1068, .y = 2, .width = 45, .height = 50 },
+            .leftStep = .{ .x = 936, .y = 2, .width = 45, .height = 50 },
+            .rightStep = .{ .x = 980, .y = 2, .width = 45, .height = 50 },
+            .crouchLeftStep = .{ .x = 1112, .y = 20, .width = 58, .height = 28 },
+            .crouchRightStep = .{ .x = 1170, .y = 20, .width = 58, .height = 28 },
+            .menuIcon = .{ .x = 40, .y = 4, .width = 43, .height = 46 },
         },
+        .groundTexture = .{ .x = 2, .y = 52, .width = 1200, .height = 14 },
+    };
+
+    var state = gameUI.GameState.startScreen;
+    var obstacles = [1]gameUI.Obstacle{
+        gameUI.Obstacle{
+            .rect = .{ .x = 800, .y = UIConfig.dinoPosition.y, .width = 100, .height = 100 },
+            .type = gameUI.ObstacleType.Cactus,
+        },
+    };
+    var lines = [2]raylib.Vector2{
+        .{ .x = 0, .y = UIConfig.dinoPosition.y + (UIConfig.dino.standing.height - UIConfig.groundTexture.height) * UIConfig.scaleFactor },
+        .{ .x = UIConfig.groundTexture.width * UIConfig.scaleFactor, .y = UIConfig.dinoPosition.y + (UIConfig.dino.standing.height - UIConfig.groundTexture.height) * UIConfig.scaleFactor },
+    };
+    var scene = gameUI.Scene{
+        .ground = gameUI.Ground{
+            .lines = &lines,
+        },
+        .obstacles = &obstacles,
     };
 
     while (!raylib.WindowShouldClose()) {
+        const deltaTime: f32 = raylib.GetFrameTime();
+
         raylib.BeginDrawing();
         raylib.ClearBackground(raylib.WHITE);
-        gameUI.DrawMenu(UIConfig);
+        gameUI.UpdateScene(&scene, &UIConfig, deltaTime);
+        gameUI.DrawUI(&state, &UIConfig, &scene);
 
         raylib.EndDrawing();
     }
